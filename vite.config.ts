f69783d1e936b62f4affc87a2dc886f7abcb6409
William Chong <me@williamchong.cloud>
2025-07-28 02:34:49 +0800

    👷 Add CD to github page

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: './src',
+  base: process.env.NODE_ENV === 'production' ? '/over-bingo/' : '/',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
